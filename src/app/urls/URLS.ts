17f465d7288891d45559e6d5823e63e0784e2e88
pavan7297 <mallapavan072@gmail.com>
2026-08-05 19:56:11 +0530

    review bug fix

--- src/app/urls/URLS.ts
+++ src/app/urls/URLS.ts
@@ -1,10 +1,10 @@
 
 export const URLS = {
 
-    backendapi: 'https://api.mbcareersolutions.in/api',
+    // backendapi: 'https://api.mbcareersolutions.in/api',
 
 
-  // backendapi: 'http://localhost:2233/api',
+  backendapi: 'http://localhost:2233/api',
   // localapi: 'http://localhost:2233/api'
 
 }
